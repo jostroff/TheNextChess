1e3584ebcecd24289714e9309ccb108a90d228f9
Valentin Mladenov <valentinvalentinovv@abv.bg>
2017-09-07 15:32:58 +0300

    Built for appharbor deploy

---
+++ Chess.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chess.Web.Startup))]
+namespace Chess.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
