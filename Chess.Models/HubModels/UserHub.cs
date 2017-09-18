94e04fbf04ef90c37d579a0c48f18694ddba8111
Valentin Mladenov <valentinvalentinovv@abv.bg>
2017-09-18 21:41:15 +0300

    Implemented SignalR invitations

---
+++ Chess.Models/HubModels/UserHub.cs
@@ -0,0 +1,11 @@
+﻿namespace Chess.Models.HubModels
+{
+    using System;
+
+    public class UserHub
+    {
+        public string Username { get; set; }
+
+        public string ConnectionId { get; set; }
+    }
+}
--- Chess.Web/Hubs/Game.cs
+++ Chess.Web/Hubs/Game.cs
@@ -1,16 +1,48 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using Chess.Game.Figures.Abstracts;
+using Chess.Game.Figures.Contracts;
+using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Threading.Tasks;
+using Chess.Models.HubModels;
 
 namespace Chess.Web.Hubs
 {
     public class Game : Hub
     {
-        public void SendMessage(string message)
+        public static List<UserHub> Users = new List<UserHub>();
+
+        public void Invite(string username)
         {
-            Clients.All.addMessage($"{ Context.User.Identity.Name }: {message}");
+            var host = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            var joinUsers = Users.Where(x => x.Username == username).ToList();
+            joinUsers.ToList().ForEach(x => 
+            {
+                Clients.Client(x.ConnectionId).recieveInvitation(host);
+            });
+        }
+
+        public override Task OnConnected()
+        {
+            var user = new UserHub
+            {
+                Username = Context.User.Identity.Name,
+                ConnectionId = Context.ConnectionId
+            };
+
+            Users.Add(user);
+            Clients.All.listUsers(Users.Select(x => x.Username).Distinct());
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var userToRemove = Users.FirstOrDefault(u => u.ConnectionId == Context.ConnectionId);
+            Users.Remove(userToRemove);
+            Clients.All.listUsers(Users.Select(x => x.Username).Distinct());
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
