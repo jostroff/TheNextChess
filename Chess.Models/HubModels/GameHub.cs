9aab17a6725f41045510055eca97326734df3f0d
Valentin Mladenov <valentinvalentinovv@abv.bg>
2017-09-19 16:44:16 +0300

    Implemented SignalR chat and chessboard drawing

---
+++ Chess.Models/HubModels/GameHub.cs
@@ -0,0 +1,20 @@
+﻿namespace Chess.Models.HubModels
+{
+    using Chess.Game.GameEngine.Contracts;
+    using System;
+    using System.Collections.Generic;
+
+    public class GameHub
+    {
+        public GameHub()
+        {
+            this.Players = new HashSet<UserHub>();
+        }
+
+        public Guid GameId { get; set; }
+
+        public IEngine GameEngine { get; set; }
+
+        public ICollection<UserHub> Players { get; set; }
+    }
+}
--- Chess.Web/Hubs/Game.cs
+++ Chess.Web/Hubs/Game.cs
@@ -7,25 +7,78 @@ using System.Linq;
 using System.Web;
 using System.Threading.Tasks;
 using Chess.Models.HubModels;
+using Chess.Game.GameEngine.Contracts;
+using Chess.Game.GameEngine;
+using Chess.Game.InputProviders.Contracts;
+using Chess.Game.InputProviders;
+using Chess.Game.GameEngine.Initializators;
+using Chess.Game.OutputProviders.Contracts;
+using Chess.Game.OutputProviders;
+using Newtonsoft.Json;
 
 namespace Chess.Web.Hubs
 {
     public class Game : Hub
     {
         public static List<UserHub> Users = new List<UserHub>();
+        public static List<GameHub> Games = new List<GameHub>();
 
         public void Invite(string username)
         {
             var host = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
             var joinUsers = Users.Where(x => x.Username == username).ToList();
-            joinUsers.ToList().ForEach(x => 
+            joinUsers.ToList().ForEach(x =>
             {
                 Clients.Client(x.ConnectionId).recieveInvitation(host);
             });
         }
 
+        public void SendConfirmation(string hostId)
+        {
+            var host = Users.FirstOrDefault(x => x.ConnectionId == hostId);
+            var guest = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            Clients.Client(host.ConnectionId).recieveConfirmation(guest);
+
+            this.StartGame(host, guest);
+        }
+
+        private void StartGame(UserHub host, UserHub guest)
+        {
+            IInputProvider inputProvider = new InputProvider();
+            IEngine engine = new TwoPlyersEngine(inputProvider);
+            IGameInitializationStrategy strategy = new TwoPlayersInitializationStrategy();
+            engine.Initialize(strategy, host.Username, guest.Username);
+            IOutputProvider outputProvider = new WebOutputProvider();
+
+            Guid gameId = Guid.NewGuid();
+            var players = new HashSet<UserHub>() { host, guest };
+            var game = new GameHub
+            {
+                GameId = gameId,
+                GameEngine = engine,
+                Players = players
+            };
+
+            Games.Add(game);
+
+            var board = outputProvider.GetBoardToRender(engine);
+
+            Clients.Client(host.ConnectionId).startGame(board);
+            Clients.Client(guest.ConnectionId).startGame(board);
+
+        }
+
+        public void SendMessageToOponent(string message, string oponentConnectionId)
+        {
+            var username = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId).Username;
+            Clients.Client(oponentConnectionId).recieveMessage(message, username);
+        }
+
         public override Task OnConnected()
         {
+            var username = Context.User.Identity.Name;
+            Clients.Client(Context.ConnectionId).getMyUsername(username);
+
             var user = new UserHub
             {
                 Username = Context.User.Identity.Name,
